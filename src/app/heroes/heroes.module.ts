dc420bbccef437941ec31d253152bbe882d06c55
Cristina Osorio <48217224+CristinaOsorio@users.noreply.github.com>
2022-07-23 22:49:34 -0500

    Creacion de componentes que se utilizaran

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CreateComponent } from './pages/create/create.component';
+import { SearchComponent } from './pages/search/search.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListComponent } from './pages/list/list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CreateComponent,
+    SearchComponent,
+    HeroeComponent,
+    HomeComponent,
+    ListComponent
+  ],
   imports: [
     CommonModule
   ]
