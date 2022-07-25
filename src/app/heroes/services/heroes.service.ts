23f1dc31291fc7761bd748068d2bf3758858a026
Cristina Osorio <48217224+CristinaOsorio@users.noreply.github.com>
2022-07-25 18:18:23 -0500

    Definicion de la url de la api en enviroment

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -3,21 +3,22 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Heroes } from '../interfaces/heroes.interface';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
-  private url = 'http://localhost:3000';
+  private api = environment.api;
 
   constructor(private http: HttpClient) { }
 
   getHeroes(): Observable<Heroes[]> {
-    return this.http.get<Heroes[]>(`${this.url}/heroes`)
+    return this.http.get<Heroes[]>(`${this.api}/heroes`)
   }
 
   getHeroe(id: string): Observable<Heroes> {
-    return this.http.get<Heroes>(`${this.url}/heroes/${id}`)
+    return this.http.get<Heroes>(`${this.api}/heroes/${id}`)
   }
 
 }
